2014e5b5808d0cf15a2b0801aac24a1516d97fa8
Shitty <85826636+Shittonsio@users.noreply.github.com>
2021-08-02 01:56:05 +0200

    Update center.cpp

--- center.cpp
+++ center.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 const int ll = 80;
 
-void center (string a, bool b) { 
+void center (string a, int b) { 
 	
 	int spaces = 0;
 	
-	if (b) {
+	if (b == 1) {
 		
 		cout << "\n";
    
     	spaces = (ll - a.size()) / 2; 
 
-    if (spaces > 0) cout << string(spaces, ' ');
+    	if (spaces > 0) cout << string(spaces, ' ');
    
     	cout << a << endl;
+    	
+	} else if(b == 2) {
+	
+    	spaces = (ll - a.size()) / 2; 
+    
+    	if (spaces > 0) cout << string(spaces, ' ');
+   
+    	cout << a;
     	
 	} else {
    
